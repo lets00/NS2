056fee1d9bc54e6830b0c5e266155752f7905795
Luís Eduardo <lets@cin.ufpe.br>
2014-05-12 09:58:48 -0300

    Add SCTP protocol and order TCP

--- SCTP/sctp_multiplesFiles.tcl
+++ SCTP/sctp_multiplesFiles.tcl
@@ -1,50 +1,55 @@
 set ns [new Simulator]
 
 $ns color 1 Blue
+$ns color 2 Red
 
-set nf [open sctp.nam w]
+set nf [open sctpMultiplesFiles.nam w]
 $ns namtrace-all $nf
 
-set TraceFile [open sctp.tr w]
+set TraceFile [open sctpMultiplesFiles.tr w]
 $ns trace-all $TraceFile
-
 
 #Create 4 nodes
 
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
-#set n3 [$ns node]
 
 $ns duplex-link $n0 $n1 400Kb 80ms DropTail
 $ns duplex-link $n1 $n2 40kb 80ms DropTail
-#$ns duplex-link $n2 $n3 10Mb 20ms DropTail
+$ns queue-limit $n1 $n2 20
 
 #SCTP
-
 set sctp0 [new Agent/SCTP]
+$sctp0 set fid_ 1
 $ns attach-agent $n0 $sctp0
 
 set sctp1 [new Agent/SCTP]
 $ns attach-agent $n2 $sctp1
 $ns connect $sctp0 $sctp1
 
-set ftp [new Application/FTP]
-$ftp attach-agent $sctp0
+#$ftp
 
-$ns at 0.1 "$ftp start"
-$ns at 50.0 "$ftp stop"
+set ftp1 [new Application/FTP]
+$ftp1 attach-agent $sctp0
+
+$ns at 0.1 "$ftp1 start"
+$ns at 0.2 "$ftp1 send 52428800"
+$ns at 0.3 "$ftp1 send 1024"
+$ns at 50.0 "$ftp1 stop"
  
 proc finish {} {
 	global ns TraceFile NamFile
 	$ns flush-trace
 	close $TraceFile
 #	close $nf
-	exec nam sctp.nam &
+	exec nam sctpMultiplesFiles &
 	exit 0
 }
 
 $ns at 55.0 "finish"
+
+#Run
 $ns run
 
 
---
+++ TCP/tcpWithFTP.tcl
@@ -0,0 +1,58 @@
+# Create a ns object
+set ns [new Simulator]
+ 
+$ns color 1 Blue
+$ns color 2 Red
+ 
+# Open the Trace files
+set TraceFile [open tcpWithFTP.tr w]
+$ns trace-all $TraceFile
+ 
+# Open the NAM trace file
+set NamFile [open tcpWithFTP.nam w]
+$ns namtrace-all $NamFile
+ 
+# Create six nodes
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+$ns duplex-link $n0 $n1 400Kb 80ms DropTail
+$ns duplex-link $n1 $n2 40Kb 80ms DropTail
+
+#TCP N0 and N4
+set tcp1 [new Agent/TCP/Newreno]
+$ns attach-agent $n0 $tcp1
+ 
+set sink1 [new Agent/TCPSink/DelAck]
+$ns attach-agent $n2 $sink1
+$ns connect $tcp1 $sink1
+ 
+$tcp1 set fid_ 1
+#$tcp1 set window_ 1000
+#$tcp1 set packetSize_ 1000
+ 
+#TCP N0 and N4
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp1
+$ftp set type_ FTP
+#$ftp1 set window_ 1000
+ 
+ 
+$ns at 0.1 "$ftp start"
+$ns at 0.2 "$ftp send 52428800"
+$ns at 0.3 "$ftp send 1024"
+$ns at 50.0 "$ftp stop"
+ 
+proc finish {} {
+	global ns TraceFile NamFile
+	$ns flush-trace
+	close $TraceFile
+	close $NamFile
+	exec nam tcpWithFTP.nam &
+	exit 0
+}
+
+$ns at 55.0 "finish"
+$ns run
